4514e1b245fdcd64d461006c595e733f561a9b82
MORJAN-CUN <janluy_moreno@cun.edu.co>
2024-05-09 23:31:30 -0500

    ajuste de vista para la bienvenida al sistema

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -1,19 +1,24 @@
 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
-<div class="container"></div>
-    <div class="row">
+    <div class="container">
+        <div class="row text-center">
         <div class="col-md-5 col-lg-5 colxl-5 mx-auto">
-            <x-jet-application-logo/>
+            <x-jet-application-logo />
         </div>
-        <div class="col-md-12 col-lg-12 col-sm-12 mt-2 text-center">
-            <h1>{{__('Welcome to your SYSGOL application!')}}</h1>
+        <div class="col-md-12 col-lg-12 col-sm-12 mt-2 ">
+            @if (Route::has('login'))
+            <div>
+                @auth
+                <a href="{{ url('/dashboard') }}" class="btn btn-danger">Dashboard</a>
+                @else
+                <a href="{{ route('login') }}" class="btn btn-danger">Log in</a>
+
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="btn btn-danger">Register</a>
+                @endif
+                @endauth
+            </div>
+            @endif
         </div>
-        <div class="col-md-12 col-lg-12 col-sm-12 mt-2 justify-content">
-            <h6>
-                SYSGOL provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
-                to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
-                you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
-                ecosystem to be a breath of fresh air. We hope you love it.
-            </h6>
         </div>
     </div>
 </div>

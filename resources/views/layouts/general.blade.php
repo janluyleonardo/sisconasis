45b0d8c403c542bdd93928f9a98c50c6bca01873
janluyleonardo <janluy.moreno@gmail.com>
2024-06-24 20:40:15 -0500

    Se ajusta favicon

--- resources/views/layouts/general.blade.php
+++ resources/views/layouts/general.blade.php
@@ -10,6 +10,7 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
+        <link rel="shortcut icon" href="{{asset('favicon.png')}}" type="image/x-icon">
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css?v='.now()->format('H.s')) }}">
         <link rel="stylesheet" href="{{ asset('css/bootstrap.css?V='.now()->format('H.s')) }}">
